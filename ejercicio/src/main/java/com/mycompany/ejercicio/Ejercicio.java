3229b3574c8364e0896f407852fb04356ddbbebf
Mireia <149465776+Miradriyo@users.noreply.github.com>
2024-12-30 14:32:07 +0000

    Add files via upload

---
+++ ejercicio/src/main/java/com/mycompany/ejercicio/Ejercicio.java
@@ -0,0 +1,16 @@
+package com.mycompany.ejercicio;
+import java.util.Scanner;
+public class Ejercicio {
+
+    public static void main(String[] args) {
+        System.out.println("Introduzca el nombre");
+        Scanner datos=new Scanner(System.in);
+        String nombre = datos.nextLine();
+        
+        System.out.println("Introduzca los apellidos");
+        String apellidos = datos.nextLine();
+        
+        System.out.println("Buenos días  "  + nombre  + apellidos );
+                
+    }
+}
